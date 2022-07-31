70fca65125b12e69cff1ccddc3f8d13eaf3ea228
vats16 <108174783+vats16@users.noreply.github.com>
2022-07-31 15:40:37 +0530

    fahrenheit to celcius

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double f , c;
+    cout<< "temperature in fahrenheit"<< endl ;
+    cin>>f;
+    c=(f-32)*5/9; 
+    cout<< "temperature in celcius"<< c <<endl;
+    
+    
+
+    return 0;
+}
