640b0a8ac2a48e44dcecda778aacc31e8e694857
vats16 <108174783+vats16@users.noreply.github.com>
2022-08-01 14:31:22 +0530

    pattern 7

---
+++ pattern7.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j =1;
+        while(j<=i){
+            cout<<i;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+
+    return 0;
+}
