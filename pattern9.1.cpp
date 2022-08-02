26a79fdb46e91bb16f60a224b04bf6f6ff718fb9
vats16 <108174783+vats16@users.noreply.github.com>
2022-08-02 17:40:19 +0530

    pattern9.1

---
+++ pattern9.1.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=i){
+            cout<<i+j-1;
+            j=j+1;
+        }
+        cout<<endl;
+        i = i+1;
+    }
+
+    return 0;
+}
