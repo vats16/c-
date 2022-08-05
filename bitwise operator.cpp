e7d9af7a8ceb011c1233645cb15a2fc8ca43cf04
vats16 <108174783+vats16@users.noreply.github.com>
2022-08-05 13:53:55 +0530

    bitwise operator

---
+++ bitwise operator.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 4;
+    int b = 6;
+    
+    cout<<"a&b is " << ( a&b )<<endl;
+    cout<<"a|b is " << ( a|b )<<endl;
+    cout<<"~a is " << ( ~a)<<endl;
+    cout<<"a^b is " << ( a^b )<<endl;
+
+    return 0;
+}
