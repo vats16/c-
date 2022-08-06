c4e9363358f9a3ddfdc522cc79a57a36acedffb7
vats16 <108174783+vats16@users.noreply.github.com>
2022-08-06 18:30:31 +0530

    Update prime or not using for loop.cpp

--- prime or not using for loop.cpp
+++ prime or not using for loop.cpp
@@ -16,7 +16,7 @@ int main()
     cout<<"Enter the value of n"<<endl;
     cin>>n;
     bool isprime=1;
-    for(int i =2;i<=n;i++){
+    for(int i =2;i<n;i++){
         if(n%i==0){
             isprime=0;
             break;
