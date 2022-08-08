2e26eb35a522054c2d62b166feab8a77c9db3ad6
vats16 <108174783+vats16@users.noreply.github.com>
2022-08-08 12:40:18 +0530

    Add files via upload

---
+++ decimal to binary code.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=0;
+    int i= 0;
+    while(n!=0){
+        int bit = n & 1;
+        ans=(bit*pow(10,i)+ans);
+        n = n>>1;
+        i++;
+    }
+    cout<<"Answer is "<<ans<<endl;
+
+    return 0;
+}
