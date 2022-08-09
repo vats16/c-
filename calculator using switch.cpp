c20b6e827d8ce92d19c8a6c79fbacb7da8d6dbdb
vats16 <108174783+vats16@users.noreply.github.com>
2022-08-09 13:56:05 +0530

    calculator using switch

---
+++ calculator using switch.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<< "enter the value of a"<<endl;
+    cin>>a;
+    
+    cout<< "enter the value of b"<<endl;
+    cin>>b;
+    
+    char op;
+    cout<< "choose operation to perform"<<endl;
+    cin>>op;
+    
+    switch(op){
+        case '+':cout<< a+b <<endl;
+        break;
+        
+        case '-':cout<< a-b <<endl;
+        break;
+        
+        case '*':cout<< a*b <<endl;
+        break;
+        
+        case '/':cout<< a/b <<endl;
+        break;
+        
+        case '%':cout<< a%b <<endl;
+        break;
+        
+        default:cout<< "enter the valid operation"<<endl;
+    }
+
+    return 0;
+}
